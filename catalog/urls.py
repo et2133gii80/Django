585444e01071b13b1e09dc4a84713ecdf32df2b3
Ilya Gaifullin <proskmax@yandex.ru>
2024-12-30 20:24:46 +0500

    dj 2 дз

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'catalog'
 
-urlpatterns = [path('home/', views.home_page, name = "home"),
-               path('contact/', views.contact_page, name = 'contact')]
+urlpatterns = [
+    path('home/', views.home_page, name = "home_page"),
+    path('contact/', views.contact_page, name = 'contact_page')
+]
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog'
 ]
 
 MIDDLEWARE = [
@@ -47,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'catalog'
+
 ]
 
 ROOT_URLCONF = 'config.urls'
