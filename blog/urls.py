a9747c276cb9dcb4d0bf8d155b0ac2c56e2ac2a2
Ilya Gaifullin <proskmax@yandex.ru>
2025-02-04 20:33:01 +0500

    dj 5

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,8 @@ app_name = BlogConfig.name
 
 urlpatterns = [ path('blog/create/', views.BlogCreateView.as_view(), name= 'blog_create'),
                 path('blog/', views.BlogListView.as_view(), name= 'blog_list'),
-                path('blog/detail/<int:pk>/', views.BlogDetailView.as_view(), name= 'blog_detail')
+                path('blog/detail/<int:pk>/', views.BlogDetailView.as_view(), name= 'blog_detail'),
+                path('blog/update/<int:pk>/', views.BlogUpdateView.as_view(), name= 'blog_update'),
+                path('blog/delete/<int:pk>/', views.BlogDeleteView.as_view(), name= 'blog_delete')
+
 ]
