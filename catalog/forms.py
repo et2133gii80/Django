dbce68a174d50c5c5ff73941037088da28aa13d5
Ilya Gaifullin <proskmax@yandex.ru>
2025-02-08 21:15:32 +0500

    dj 2(1)

---
+++ catalog/forms.py
@@ -0,0 +1,59 @@
+from django import forms
+from .models import Product
+from django.core.exceptions import ValidationError
+
+class ProductForm(forms.ModelForm):
+    forbidden_words  = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super(ProductForm, self).__init__(*args, **kwargs)
+
+        self.fields['product_name'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите название товара'
+        })
+
+        self.fields['description'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите описание товара'
+        })
+
+        self.fields['img'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Загрузите изображение'
+        })
+
+        self.fields['category'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Выберете категорию'
+        })
+
+        self.fields['price'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Укажите цену'
+        })
+
+    def clean_product_name(self):
+        product_name = self.cleaned_data.get('product_name')
+        if product_name.lower() in self.forbidden_words:
+            raise ValidationError('запрещенное слово')
+        return product_name
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+        if description.lower() in self.forbidden_words:
+            raise ValidationError('запрещенное слово')
+        return description
+
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if price <= 0:
+            raise ValidationError('цена некорректная')
+        return price
+
+
+
+
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.utils.translation.trans_real import catalog
 from mypy.types import names
 
 from . import views
@@ -8,7 +9,10 @@ app_name = 'catalog'
 
 urlpatterns =[ path('', ProductListView.as_view(), name = 'product_list'),
                path('catalog/<int:pk>/', ProductDetailView.as_view(), name = 'products_detail'),
-               path('contact/', ContactTemplateView.as_view(), name = 'contact_page')
+               path('contact/', ContactTemplateView.as_view(), name = 'contact_page'),
+               path('catalog/create/', views.ProductCreateView.as_view(), name = 'product_create'),
+               path('catalog/update/<int:pk>/', views.ProductUpdateView.as_view(), name = 'product_update'),
+               path('catalog/delete/<int:pk>/', views.ProductDelete.as_view(), name = 'product_delete')
 
 ]
 
