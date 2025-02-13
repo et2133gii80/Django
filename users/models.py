325319469b3b122c1ba0c6223c0f23595aa7e2cf
Ilya Gaifullin <proskmax@yandex.ru>
2025-02-14 01:01:51 +0500

    dj 3

---
+++ users/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    avatar = models.ImageField(upload_to='users/avatars/', blank=True, null=True)
+    phone_number = models.CharField(max_length= 15, blank=True, null=True)
+    country = models.CharField(max_length=30, blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
