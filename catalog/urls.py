3ce1f74948b6c7729ef055b270a1952701adba9a
Ilya Gaifullin <proskmax@yandex.ru>
2025-02-27 17:59:18 +0500

    dj 5

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,23 +1,16 @@
 from django.urls import path
-from django.utils.translation.trans_real import catalog
-from mypy.types import names
-
+from django.views.decorators.cache import cache_page
 from . import views
 from .views import ProductListView,ContactTemplateView,ProductDetailView
 
 app_name = 'catalog'
 
 urlpatterns =[ path('', ProductListView.as_view(), name = 'product_list'),
-               path('catalog/<int:pk>/', ProductDetailView.as_view(), name = 'products_detail'),
+               path('catalog/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name = 'products_detail'),
                path('contact/', ContactTemplateView.as_view(), name = 'contact_page'),
                path('catalog/create/', views.ProductCreateView.as_view(), name = 'product_create'),
                path('catalog/update/<int:pk>/', views.ProductUpdateView.as_view(), name = 'product_update'),
-               path('catalog/delete/<int:pk>/', views.ProductDelete.as_view(), name = 'product_delete')
+               path('catalog/delete/<int:pk>/', views.ProductDelete.as_view(), name = 'product_delete'),
+               path('category/<int:pk>/', views.ProductsByCategoryView.as_view(), name='products_by_category')
 
 ]
-
-# urlpatterns = [
-#     path('home/', HomePageListView.as_view(), name = "home_page"),
-#     path('contact/', ContactTemplateView.as_view(), name = 'contact_page'),
-#     path('product_info/', views.product_info, name = 'product_info')
-# ]
